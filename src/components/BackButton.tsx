1556b2aef5c26e3dd3ad4b3f04186fc42d64512b
timechamber <timechamber8@gmail.com>
2021-11-05 17:51:42 -0700

    Added back button to farm, add/remove liquidity pages

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from "react"
+import { Link } from "react-router-dom"
+import styles from "./BackButton.module.scss"
+
+interface Props {
+  wrapperClass: string
+  route: string
+  buttonText: string
+}
+
+export default function BackButton(props: Props): ReactElement {
+  const { wrapperClass, route, buttonText } = props
+  return (
+    <div className={wrapperClass}>
+      <Link to={route} className={styles.arrowLeft}>
+        <i>{buttonText}</i>
+      </Link>
+    </div>
+  )
+}
