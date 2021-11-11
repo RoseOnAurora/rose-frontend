fbe4ca32c2311586a5f22f522b8934e639fffc11
timechamber <timechamber8@gmail.com>
2021-11-10 19:15:34 -0800

    Added row for total pool token balance on liquidity pages

--- src/components/MyShareCard.tsx
+++ src/components/MyShareCard.tsx
@@ -22,8 +22,11 @@ function MyShareCard({ data }: Props): ReactElement | null {
 
   const formattedData = {
     share: formatBNToPercentString(data.share, 18),
+    shareAmount: commify(
+      formatBNToString(data.underlyingTokensAmount, 18, formattedDecimals),
+    ),
     usdBalance: commify(formatBNToString(data.usdBalance, 18, 2)),
-    amount: commify(
+    lpTokenBalance: commify(
       formatBNToString(data.lpTokenBalance, 18, formattedDecimals),
     ),
     amountsStaked: Object.keys(data.amountsStaked).reduce((acc, key) => {
@@ -64,8 +67,12 @@ function MyShareCard({ data }: Props): ReactElement | null {
           <span className="value">{`$${formattedData.usdBalance}`}</span>
         </div>
         <div className="infoItem">
+          <span className="label bold">{`${t("poolTokenBalance")}: `}</span>
+          <span className="value">{`${formattedData.shareAmount}`}</span>
+        </div>
+        <div className="infoItem">
           <span className="label bold">{`${t("lpTokenBalance")}: `}</span>
-          <span className="value">{formattedData.amount}</span>
+          <span className="value">{formattedData.lpTokenBalance}</span>
           {Object.keys(data.amountsStaked).map((key) => {
             return data.amountsStaked[key as Partners]?.gt(Zero) ? (
               <span className="value">
