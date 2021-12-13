5c6f21848ad693c717919ba1fa12de09fe09a925
timechamber <timechamber8@gmail.com>
2021-12-13 11:42:45 -0800

    Open socials in new tab

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -7,19 +7,35 @@ function BottomMenu(): ReactElement | null {
   return (
     <div className={styles.bottommenu}>
       <div className={styles.socials}>
-        <a href="https://twitter.com/roseonaurora">
+        <a
+          href="https://twitter.com/roseonaurora"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaTwitter />
         </a>
-        <a href="https://medium.com/@roseonaurora">
+        <a
+          href="https://medium.com/@roseonaurora"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsMedium />
         </a>
-        <a href="https://t.me/RoseOnAurora">
+        <a href="https://t.me/RoseOnAurora" target="_blank" rel="noreferrer">
           <FaTelegram />
         </a>
-        <a href="https://discord.gg/dG6mWH4rHj">
+        <a
+          href="https://discord.gg/dG6mWH4rHj"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaDiscord />
         </a>
-        <a href="https://github.com/RoseOnAurora">
+        <a
+          href="https://github.com/RoseOnAurora"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaGithub />
         </a>
       </div>
